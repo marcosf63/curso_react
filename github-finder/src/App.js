614a6846b3296705ca10486755722c5f01ea39e7
Marcos Oliveira <marcosf63@gmail.com>
2020-03-11 06:20:09 -0300

    Primeiro commit

---
+++ github-finder/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navabar'
+import UserItem from './components/users/UserItem'
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  } 
+}
+
+export default App;
+ 
